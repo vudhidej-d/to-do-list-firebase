86d871f5b4dee0e2e63b3df83ccc2647764bb8db
Ton Vudhidej <vudhidej@muze.co.th>
2019-02-07 00:21:34 +0700

    create Task component

---
+++ src/components/Task.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Col, Card } from 'antd'
+
+const Task = (props) => {
+  const { id, task } = props
+
+  return (
+    <Col span={24} style={{ background: '#ECECEC', padding: '0px 40px' }}>
+      <Card key={id}>
+        {task}
+      </Card>
+    </Col>
+  )
+}
+
+export default Task
